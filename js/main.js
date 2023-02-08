7c9a0f22c9f7a18b0baeba16f9118bc2c0fad354
Marlon Santos <49647245+maarlonsantos@users.noreply.github.com>
2023-02-08 11:15:12 -0300

    ajusta array e inseri favicon

--- js/main.js
+++ js/main.js
@@ -22,14 +22,21 @@ function divide(a,b) {
   return a/b;
 }
 
-function pegaInfoForm() {
-  var nome = document.getElementById('nome-form').value;
-  var email = document.getElementById('email-form').value;
-  var msg = document.getElementById('msg-form').value;
-  var dados = [nome,email,msg];
-  return console.write(dados);
-}
-
-// function enviaArray() {
-//   document.getElementById('btn-envia').onclick()
+// function pegaInfoForm() {
+//   var nome = document.getElementById('nome-form').value;
+//   var email = document.getElementById('email-form').value;
+//   var msg = document.getElementById('msg-form').value;
+//   var dados = [nome,email,msg];
+//   return console.write(dados);
 // }
+
+var nome = document.getElementById('nome-form');
+var email = document.getElementById('email-form');
+var msg = document.getElementById('msg-form');
+var dados = [nome,email,msg];
+
+document.querySelector('form.formContato').addEventListener('submit', function (e) {
+    //previne o envio do formulario
+    e.preventDefault();
+    console.log([nome.value,email.value,msg.value]);    
+});
